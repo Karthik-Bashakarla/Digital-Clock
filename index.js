8e0ab3a7ebb260501114a2637828b936004ba136
Karthik Bashakarla <53942291+Karthik-Bashakarla@users.noreply.github.com>
2021-06-22 02:07:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const seconds = document.getElementById("sec");
 
 const getCurrentTime = () => {
   let date = new Date();
-  let localTime = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second:'2-digit' });
+  let localTime = date.toLocaleTimeString([], { hour12: false });
 
     let [hr1, hr2, , min1, min2, , sec1, sec2] = localTime;
     hours.innerText = hr1 + hr2;
